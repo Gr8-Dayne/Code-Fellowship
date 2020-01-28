2aafe229a15def311fe84bb3b49ea064e65071d2
Dayne Daylong <dalexad@gmail.com>
2020-01-28 11:09:06 -0800

    init

---
+++ src/main/java/com/daylong/CodeFellowship/CodeFellowshipApplication.java
@@ -0,0 +1,13 @@
+package com.daylong.CodeFellowship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFellowshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFellowshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daylong/CodeFellowship/CodeFellowshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daylong.CodeFellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFellowshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
