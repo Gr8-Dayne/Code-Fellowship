255d0f76f408ea9fda6a0f9431ff0536816648d3
Dayne Daylong <dalexad@gmail.com>
2020-02-04 10:51:15 -0800

    fixed db issue

--- src/main/java/com/daylong/CodeFellowship/CodeFellowshipApplication.java
+++ src/main/java/com/daylong/CodeFellowship/CodeFellowshipApplication.java
@@ -3,11 +3,8 @@ package com.daylong.CodeFellowship;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CodeFellowshipApplication {
-
+@SpringBootApplication public class CodeFellowshipApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CodeFellowshipApplication.class, args);
 	}
-
 }
--- src/test/java/com/daylong/CodeFellowship/CodeFellowshipApplicationTests.java
+++ src/test/java/com/daylong/CodeFellowship/CodeFellowshipApplicationTests.java
@@ -1,13 +1,40 @@
 package com.daylong.CodeFellowship;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class CodeFellowshipApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+//	@Autowired
+//	private ApplicationUserController applicationUserController;
 
 	@Test
 	void contextLoads() {
+//		assert that
 	}
+
+//	@Test
+//	public void homePageMVCTest() throws Exception{
+//		this.mockMvc.perform(get("/"))
+//				.andDo(print())
+//				.andExpect(status().isOk())
+//				.andExpect(content().string(containsString("<h2>Make users here</h2>")))
+//				.andExpect(content().string(containsString("<h2>Make users here</h2>")))
+//				.andExpect(content().string(containsString("<h2>Make users here</h2>")));
+
+//	}
 
 }
